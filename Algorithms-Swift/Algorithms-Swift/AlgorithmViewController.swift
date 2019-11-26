33ce68a730f1e502954286fa5b9ee6cd3cd3b53a
nabil.safatli <safatli.nabil@gmail.com>
2019-11-26 22:48:28 +0100

    Create an algorithm table view and view controller to support testing multiple algorithms in a single application

---
+++ Algorithms-Swift/Algorithms-Swift/AlgorithmViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AlgorithmViewController.swift
+//  Algorithms-Swift
+//
+//  Created by Nabil Safatli on 2019-11-26.
+//  Copyright © 2019 Nabil. All rights reserved.
+//
+
+import UIKit
+
+class AlgorithmViewController: UIViewController {
+    var algorithmCode: (() -> Void)?
+    var algorithmName: String?
+
+    @IBOutlet weak var executeButton: UIButton! {
+        didSet {
+            executeButton.isHidden = algorithmCode == nil
+        }
+    }
+    @IBOutlet weak var algorithmNameLabel: UILabel! {
+        didSet {
+            algorithmNameLabel.text = algorithmName
+        }
+    }
+    
+    @IBAction func executeButtonTapped(_ sender: Any) {
+        algorithmCode?()
+    }
+}
--- Algorithms-Swift/Algorithms-Swift/ViewController.swift
+++ Algorithms-Swift/Algorithms-Swift/ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let sectionsTitles: [String] = ["Sorting", "Data Structure", "Challenges"]
+    let sortingAlgorithmsTitles: [String] = ["Quick Sort", "Bubble Sort", "Merge Sort"]
+    let dataStructuresTitles: [String] = ["Binary Tree", "Graph", "Queue", "Stack"]
+    let challengesTitles: [String] = ["Two Sum", "Binary Search", "Rotate Left Array"]
+    var algorithmsGroups: [[String]] {
+        return [sortingAlgorithmsTitles, dataStructuresTitles, challengesTitles]
     }
 
-    @IBAction func executeAlgorithm(_ sender: Any) {
-        QuickSort.execute()
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return algorithmsGroups[section].count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        let algorithmName = algorithmsGroups[indexPath.section][indexPath.row]
+        cell.textLabel?.text = algorithmName
+        return cell
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return algorithmsGroups.count
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionsTitles[section]
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard indexPath.section == 0, let
+            storyboardController = self.storyboard?.instantiateViewController(withIdentifier: "AlgorithmViewController"),
+            let algorithmViewController = storyboardController as? AlgorithmViewController else { return }
+
+        if(indexPath.row == 0) {
+            algorithmViewController.algorithmName = "Quick Sort"
+            algorithmViewController.algorithmCode = {
+                QuickSort.execute()
+            }
+        } else if(indexPath.row == 1) {
+            algorithmViewController.algorithmName = "Bubble Sort"
+            algorithmViewController.algorithmCode = {
+                BubbleSort.execute()
+            }
+        } else {
+            algorithmViewController.algorithmName = "Work in Progress"
+        }
+
+        self.navigationController?.pushViewController(algorithmViewController, animated: true)
     }
 }
