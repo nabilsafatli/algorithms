fcc766d111436f86a7f4419ba30a30bb302a79a7
nabil.safatli <safatli.nabil@gmail.com>
2019-12-24 22:25:29 +0100

    Create storyboard and inteface to write algorithm for autocomplete test

---
+++ Algorithms-Swift/Algorithms-Swift/SystemDesign/AutoCompleteSearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AutoCompleteSearchViewController.swift
+//  Algorithms-Swift
+//
+//  Created by Nabil Safatli on 2019-12-24.
+//  Copyright © 2019 Nabil. All rights reserved.
+//
+
+import UIKit
+
+class AutoCompleteSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
